d37f8bcf9bf7bcd3746f3b7f43edfb28ac16ab91
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-09-21 08:20:18 -0700

    create function to convert choosen word to hyphens and store in array that the user will see

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,27 +9,51 @@ var placeholder = [];
 //if it does then examine the indices of the letter occurance(s) and replace the '_' with letter on placeholder
 // assign the value of the placeholder to a variable and send it to html
 
+// *********FUNCTIONS ********
 
 // computer selects word from word bank
-function randomWordChoice(){
+function randomWordChoice() {
   var veganWordBank= ["kale", "bean", "pear", "peanut"];
 
   var veganWordchoice = veganWordBank[Math.floor(Math.random() * veganWordBank.length)];
 
-    return veganWordchoice;
+  return veganWordchoice;
   };
 
   
-  // Convert word into comma separated format
+  // Convert word into comma separated format & hyphens
   function selectedWordToArray(){
     var word = randomWordChoice();
+    // maybe use a join *********
+    toHyphens(word);
 
     selectedWord= Array.from(word);
     
     return selectedWord;
+    console.log(word);
   }
-    console.log(selectedWordToArray());
-    console.log(selectedWord);
+  
+
+    // use a for loop to iterate through eat letter, validate its not already been guessed and push to its position on the placeholder array.
+  function loopThroughWord(letter){
+    for (var i = 0; i < selectedWord.length; i++){
+      if(selectedWord[i] === letter ){
+        placeholder.splice(i, 0, letter);
+      }
+    }
+  };
+
+ function toHyphens(wordToConvert) {
+  for (var i= 0; i < wordToConvert.length; i++){
+    placeholder.push("-")
+    }
+
+  };
+
+// ******** MAIN SECTION**********
+
+selectedWordToArray();
+console.log(selectedWord);
 
     // on each keystroke, check 1.) has this letter been guessed? If YES alert user. If NO push userGuess to array so that the user can see they guessed the letter. also if YES 
   document.onkeyup=function(event){
@@ -43,24 +67,16 @@ function randomWordChoice(){
     else {
       alert("already guessed that letter")
     }
-    console.log(guesses);
-    console.log(placeholder);
-    console.log(selectedWord);
+    console.log("guesses" + guesses);
+    console.log("hypens" + placeholder);
+    console.log("selected word" + selectedWord);
   };
 
    
 
-// use a for loop to iterate through eat letter, validate its not already been guessed and push to its position on the placeholder array.
-function loopThroughWord(letter){
-  for (var i = 0; i < selectedWord.length; i++){
-    if(selectedWord[i] === letter ){
-      placeholder.splice(i, 0, letter);
-    }
-  }
-};
-
+// create a function to turn word into hyphens
 // Need to create a reset function
-
+ 
 
 
 
